0561e5a361cdb2e1b9434a153560b36c1b749316
colinrodney <bigboi0000@gmail.com>
2026-01-26 07:23:00 -0600

    TESTING DATA INGESTION FUNCTIONALITY - added .csv file to be read in + added ingest_data.py file + modified main.py to import + execute ingest_data module + show dataframe info.

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -5,7 +5,10 @@ import test_directory
 # Import modules
 import test_directory.module1
 import test_directory.module2
+import test_directory.ingest_data.py
 
 # Invoke functions
 test_directory.module1.func1()
 test_directory.module2.func2()
+df_2 = test_directory.module2.ingest_data()
+df_2.info()
