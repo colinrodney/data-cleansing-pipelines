2164282d551ddfa18775c3df44da8c2de5149520
colinrodney <bigboi0000@gmail.com>
2026-01-27 05:37:23 -0600

    Removed all library import attempts and pandas related functions from ingest_data() function and having it only print a message to confirm it is running successfully

--- test_directory/ingest_data.py
+++ test_directory/ingest_data.py
@@ -1,7 +1,7 @@
 def ingest_data():
-    import pandas as pd
-    df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
-    df_2 = df.copy()
-    print("Data ingested successfully!")
-    df_2.info()
-    return df_2
+    # import pandas as pd
+    # df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
+    # df_2 = df.copy()
+    print("ingest data function called successfully!")
+    # df_2.info()
+    # return df_2
