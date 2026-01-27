1048b3d526b0509af694440739970e3b68bf2d2e
colinrodney <bigboi0000@gmail.com>
2026-01-26 18:48:53 -0600

    moved df_2.info() function call from main.py and placed it inside ingest_data() function.

--- test_directory/ingest_data.py
+++ test_directory/ingest_data.py
@@ -3,4 +3,5 @@ def ingest_data():
     df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
     df_2 = df.copy()
     print("Data ingested successfully!")
+    df_2.info()
     return df_2
