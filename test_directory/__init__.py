841c557bcce88c89fdd06373d9cdfa78a9b4c7a5
colinrodney <bigboi0000@gmail.com>
2026-01-26 18:28:52 -0600

    Added ingest_data module to __all__ variable and commanded python to import ingest_data module from __init__.py file.

--- test_directory/__init__.py
+++ test_directory/__init__.py
@@ -2,9 +2,10 @@
 # __all__ variable  = list of strings indicating names of modules
 # to be imported when using the *operator
 
-__all__ = ["module1", "module2"]
+__all__ = ["module1", "module2", "ingest_data"]
 
 # Import submodules
 from . import module1
 from . import module2
+from . import ingest_data
 
