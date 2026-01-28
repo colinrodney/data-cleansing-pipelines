e49d1910dff5b1d3b22a19a8ffe69efa1eb44c09
colinrodney <bigboi0000@gmail.com>
2026-01-28 05:31:31 -0600

    Adjusted path of .csv file to be ingested for processing (.csv file now located at root of test_directory alongside ingest_data module).

--- test_directory/ingest_data.py
+++ test_directory/ingest_data.py
@@ -5,7 +5,7 @@ def ingest_data():
         except:
              print("EXCEPTION - Pandas import not successfull.")
         else:
-            df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
+            df = pd.read_csv("dirty_cafe_sales-csv.csv")
             df_2 = df.copy()
             df_2.info()
             print("ingest data function ran successfully! RETURNING file to function!")
