d19081bd378f06f8fee797e8754231f675e083f9
colinrodney <bigboi0000@gmail.com>
2026-01-28 05:14:47 -0600

    Re-add code to import Pandas library after addressing .yml file steps order issues. Also add try/except/else structure to gracefully handle any errors.

--- test_directory/ingest_data.py
+++ test_directory/ingest_data.py
@@ -1,7 +1,12 @@
 def ingest_data():
-    # import pandas as pd
-    # df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
-    # df_2 = df.copy()
-    print("ingest data function called successfully!")
-    # df_2.info()
-    # return df_2
+        try:
+            import pandas as pd
+            print("Pandas imported successfully!")
+        except:
+             print("EXCEPTION - Pandas import not successfull.")
+        else:
+            df = pd.read_csv(r"C:\Users\Cessn\OneDrive\Desktop\data_cleansing_CI_CD_pipeline\.github\workflows\dirty_cafe_sales-csv.csv")
+            df_2 = df.copy()
+            df_2.info()
+            print("ingest data function ran successfully! RETURNING file to function!")
+            return df_2
